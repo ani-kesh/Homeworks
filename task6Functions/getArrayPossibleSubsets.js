73cca0893668aa6aa87b2920a32c1fdc2a74b42b
ani-kesh <aniqeshishyan1992@gmail.com>
2021-05-06 10:05:45 +0400

    fix:added 13 task

--- task6Functions/getArrayPossibleSubsets.js
+++ task6Functions/getArrayPossibleSubsets.js
@@ -6,9 +6,21 @@ elements in the array are unique.
 -1], [9, 23, 0], [9, 23, -2], [9, 23, -1], [9, 0, -2], [9, 0, -1], [9, -2, -1], [23, 0, -2], [23, 0, -1], [23, -2,
 -1], [0, -2,-1]]
 */
-let arrays = getArrayPossibleSubsets([5, 9, 23, 0, -2, -1]);
 
 function getArrayPossibleSubsets(array) {
-
-    return arrays;
+  let arrays = [];
+  for (let i = 0; i < array.length; i++) {
+    for (let j = i + 1; j < array.length; j++) {
+      for (let k = j + 1; k < array.length; k++) {
+        let arrayElements = [];
+        arrayElements.push(array[i]);
+        arrayElements.push(array[j]);
+        arrayElements.push(array[k]);
+        arrays.push(arrayElements);
+      }
+    }
+  }
+  return arrays;
 }
+let arrays = getArrayPossibleSubsets([5, 9, 23, 0, -2, -1]);
+console.log(arrays);
