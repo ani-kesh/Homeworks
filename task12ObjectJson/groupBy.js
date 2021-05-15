3ee3cb9fbed511f4adb857e3b4322d20bb17ffe9
ani-kesh <aniqeshishyan1992@gmail.com>
2021-05-15 22:39:53 +0400

    fix: added group by key function v2

--- task12ObjectJson/groupBy.js
+++ task12ObjectJson/groupBy.js
@@ -16,18 +16,26 @@ function groupBy(arr, fn) {
     .filter(
       (el, index) => index - 1 !== undefined && keys[index] !== keys[index - 1]
     );
+      
+  // let obj = {};
+  // for (let key of filteredCase) {
+  //   let values = [];
+  //   for (let value of arr) {
+  //     if (key === fn(value)) {
+  //       values.push(value);
+  //     }
+  //   }
+  //   obj[key] = values;
+  // }
 
-  let obj = {};
-  for (let key of filteredCase) {
-    let values = [];
-    for (let value of arr) {
-      if (key === fn(value)) {
-        values.push(value);
-      }
-    }
-    obj[key] = values;
-  }
+  let obj = filteredCase.reduce((acc, el) => {
+    return { ...acc, [el]:  arr.filter((element)=> el === fn(element))};
+  }, {});
+
   return obj;
 }
+
+
+
 
 console.log(groupBy(["one", "two", "three"], (i) => i.length));
